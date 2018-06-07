385642f528bee6e81e00857b1adaa33586962819
Fredrik Jonsén <fredrik.jonsen@cgi.com>
2018-06-07 11:10:33 +0200

    Revert "Revert "Completed and fairly optimized""
    This reverts commit a3048e0147cc704ae8947ab2b9ef4f92efbb78b1.

--- ProblemC/ProblemC/ProblemC.h
+++ ProblemC/ProblemC/ProblemC.h
@@ -2,7 +2,6 @@
 
 #include <climits>
 
-
 struct Point {
 	int x;
 	int y;
@@ -14,8 +13,6 @@ struct PointAttributes {
 };
 
 struct PointQueueItem {
-	PointQueueItem(int p, int i) : indice(i), priority(p) {}
-
 	int priority;
 	int indice;
 };
@@ -23,6 +20,6 @@ struct PointQueueItem {
 int FindPath(const int nStartX, const int nStartY,
 	const int nTargetX, const int nTargetY,
 	const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
-	int* pOutBuffer, const int nOutBufferSize, PointAttributes* point_attributes);
+	int* pOutBuffer, const int nOutBufferSize);
 
 void get_neighbors(int* const neighbors, const int currentIndice, const int nMapHeight, const int nMapWidth);
